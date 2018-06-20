a064160f29628c1012f1b859a3b95924648d7d52
Peter Nielson <computergeekpete@gmail.com>
2018-06-20 13:55:13 -0600

    Removed valgrind from script

--- travis.sh
+++ travis.sh
@@ -6,7 +6,7 @@ echo
 echo "Running clang-tidy on CPU.cc" `clang-tidy CPU.cc -- -Imy_project/include -DMY_DEFINES ...`
 echo
 echo 
-echo "Running valgrind on ./a.out" `valgrind --leak-check=yes ./a.out`
+##echo "Running valgrind on ./a.out" `valgrind --leak-check=yes ./a.out`
 echo
 echo
 ./a.out
