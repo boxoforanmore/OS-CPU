23565a47965119c863e479e71caea4056a4a355b
Peter Nielson <computergeekpete@gmail.com>
2018-06-27 13:34:09 -0600

    Changed travis script to use non-default output compiled code (CPU)

--- travis.sh
+++ travis.sh
@@ -1,12 +1,13 @@
 #/bin/bash
 echo
 echo
-echo "Running: g++ CPU.cc -DEBUG -Wall"  `g++ CPU.cc -DEBUG -Wall`
+echo "Running: g++ CPU.cc -DEBUG -Wall"  `g++ CPU.cc -DEBUG -Wall -o CPU`
+echo
 echo
 echo "Running clang-tidy on CPU.cc" `clang-tidy CPU.cc -- -Imy_project/include -DMY_DEFINES ...`
 echo
 echo 
-echo "Running valgrind on ./a.out" `valgrind --leak-check=yes ./a.out`
+echo "Running valgrind on ./CPU" `valgrind --leak-check=yes ./CPU`
 echo
 echo
-./a.out
+./CPU
