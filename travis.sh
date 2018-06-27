d9ad64e14b0ac50a28b8d646644b7218bb9affe6
Peter Nielson <computergeekpete@gmail.com>
2018-06-27 13:39:59 -0600

    Corrected echoed statement to match command

--- travis.sh
+++ travis.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 echo
 echo
-echo "Running: g++ CPU.cc -DEBUG -Wall"  `g++ CPU.cc -DEBUG -Wall -o CPU`
+echo "Running: g++ CPU.cc -DEBUG -Wall -o CPU"  `g++ CPU.cc -DEBUG -Wall -o CPU`
 echo
 echo
 echo "Running clang-tidy on CPU.cc" `clang-tidy CPU.cc -- -Imy_project/include -DMY_DEFINES ...`
